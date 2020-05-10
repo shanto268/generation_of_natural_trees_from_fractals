9ec658e5dca5fba1920f42d1ecc388f411d677a8
Sadman Ahmed Shanto <sadman-ahmed.shanto@ttu.edu>
2020-05-09 23:43:58 -0500

    initial push

---
+++ natural_selection.py
@@ -0,0 +1,33 @@
+import turtle as tu 
+import random
+foo = tu.Turtle()
+foo.speed(0) #fastest
+foo.sety(-40)
+
+def randomMove():
+    rotate = random.randint(0,360)
+    move = random.randint(-50,50)
+    foo.seth(rotate)
+    foo.forward(move)
+
+def moveRandomly():
+    time = random.randint(20,23)
+    for i in range(time):
+        random()
+
+def yTree(num):
+    factor = random.uniform(0,1)
+    if (num < 1):
+        return 
+    else:
+        foo.fd(num)
+        foo.left(30)
+        yTree(num*factor)
+        foo.right(60)
+        yTree(num*factor)
+        foo.left(30)
+        foo.bk(num)
+
+foo.left(90)
+yTree(100)
+tu.mainloop()
