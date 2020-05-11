825a86420ca1bbe929b3eb06b740a2f9ec195e51
Sadman Ahmed Shanto <sadman-ahmed.shanto@ttu.edu>
2020-05-11 00:02:48 -0500

    update

--- natural_selection.py
+++ natural_selection.py
@@ -2,7 +2,8 @@ import turtle as tu
 import random
 foo = tu.Turtle()
 foo.speed(0) #fastest
-foo.sety(-150)
+foo.penup()
+foo.setpos(0,-275)
 
 def randomMove():
     rotate = random.randint(0,360)
@@ -17,7 +18,12 @@ def moveRandomly():
 
 def yTree(num):
     factor = random.uniform(0,1)
-    angle = random.randint(30,32)
+    angle = random.randint(0,32)
+    print("factor: ", factor)
+    print("angle: ", angle)
+    print("steps: ", num)
+    print("y coordinate:", foo.ycor())
+    print()
     if (num < 1):
         return 
     else:
@@ -29,8 +35,37 @@ def yTree(num):
         foo.left(angle)
         foo.bk(num)
 
+
+#still need to fix the y splitting
+def notSoYtree(num):
+    factor = random.uniform(0,1)
+    angle = random.randint(0,32)
+    prob = random.uniform(0, 1)
+    xRand = random.uniform(0,1)
+    print("factor: ", factor)
+    print("angle: ", angle)
+    print("steps: ", num)
+    print("y coordinate:", foo.ycor())
+    print()
+    if (num < 1):
+        return 
+    else:
+        leftBranch(num,angle)
+        notSoYtree(num*factor)
+        rightBranch(num, angle,factor)
+
+def leftBranch(num,angle):
+        foo.fd(num)
+        foo.left(angle)
+
+def rightBranch(num,angle,factor):
+        foo.right(2*angle)
+        notSoYtree(num*factor)
+        foo.left(angle)
+        foo.bk(num)
+
 foo.ht()
 foo.left(90)
-yTree(150)
-#moveRandomly()
+foo.pendown()
+notSoYtree(150)
 tu.mainloop()
